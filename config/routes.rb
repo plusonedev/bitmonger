e91efd2ce8ca4f6b7b35ab428788c123d07cec67
Mike Kelly <kellysystemsrepair@gmail.com>
2020-12-12 23:51:40 -0500

    fleshed out articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'welcome/index'
+  get "/articles", to: "articles#index"
+  get "/welcome", to: "welcome#index"
 
   root 'welcome#index'
 end
