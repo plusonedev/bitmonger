22471a69567ed3ede8cc80f7e9c5af90394b8530
Mike Kelly <kellysystemsrepair@gmail.com>
2020-12-15 21:01:19 -0500

    checking javascript compilation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -22,3 +23,8 @@ import "../styles/application"
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(document).on('turbolinks:load', function () {
+  alert("We made it!");
+})
+import "controllers"
